2df1f6f39ae1b4d22585815314145a7dc702b107
Mustafa-Lutaaya <you@example.com>
2024-10-17 21:18:53 +0200

    Go

---
+++ tenv/src/toast.py
@@ -0,0 +1,12 @@
+
+
+from win10toast import ToastNotifier
+
+toast = ToastNotifier()
+
+toast.show_toast(
+    "You have a new notification!",
+    "Hello ReDi class!!",
+    duration = 5,
+    threaded = True,
+)
